1a02c940f56c8499a0fd6b6da3e3ecaf6de759ab
Arturo Guzman <arturo@guzart.com>
2017-03-29 19:49:38 -0400

    chore: add test for advanced customization

--- test/preset_test.js
+++ test/preset_test.js
@@ -1,5 +1,4 @@
 import test from 'ava';
-import { validate } from 'webpack';
 import { Neutrino } from 'neutrino';
 
 test('loads preset', t => {
@@ -8,6 +7,19 @@ test('loads preset', t => {
 
 test('uses preset', t => {
   const api = Neutrino();
-
   t.notThrows(() => api.use(require('..')));
+});
+
+test.only('supports custom properties', t => {
+  const api = Neutrino();
+  api.use(require('..'));
+
+  api.config.module
+    .rule('lint')
+    .use('eslint')
+    .tap(options => Object.assign(options, { envs: ['browser', 'mocha'] }));
+  
+  const outputConfig = api.config.toConfig();
+  const moduleOptions = outputConfig.module.rules[0].use[0].options;
+  t.deepEqual(moduleOptions.envs, ['browser', 'mocha'])
 });
